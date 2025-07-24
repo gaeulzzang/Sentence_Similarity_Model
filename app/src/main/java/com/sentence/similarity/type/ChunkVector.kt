fb7f9b6ce9dd1eb01d1d1af6db3256ff0f9dd18f
Lee Gaeul <91470334+gaeulzzang@users.noreply.github.com>
2025-07-24 10:26:42 +0900

    Add FloatArray override functions

--- app/src/main/java/com/sentence/similarity/type/ChunkVector.kt
+++ app/src/main/java/com/sentence/similarity/type/ChunkVector.kt
@@ -4,4 +4,24 @@ data class ChunkVector(
     val id: Int,
     val content: String,
     val embedding: FloatArray
-)
+) {
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as ChunkVector
+
+        if (id != other.id) return false
+        if (content != other.content) return false
+        if (!embedding.contentEquals(other.embedding)) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = id
+        result = 31 * result + content.hashCode()
+        result = 31 * result + embedding.contentHashCode()
+        return result
+    }
+}
